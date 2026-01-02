28ccbb452f7feb1d1c668e6a5e853f7ad5ebee23
Thami <sainttsquared@gmail.com>
2026-01-02 10:01:50 +0200

    change link button

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,23 +3,16 @@ import ThemeToggle from './ThemeToggle'
 import useGlobalState from './context/useGlobalState';
 import CardContainer from './CardContainer';
 import AuthForm from './AuthForm';
-import type { TFormType } from './types/types';
+import LoginLinks from './LoginLinks';
 
 
 
 function App() {
-  const { isDarkTheme, isLoginModalOpen, setIsLoginModalOpen, setMode } = useGlobalState()
-  const handleClick = (mode: TFormType) => {
-    setMode(mode)
-    setIsLoginModalOpen(true)
-  }
-
-
+  const { isDarkTheme, isLoginModalOpen} = useGlobalState()
+ 
   return (
     <main className={`${styles.main} ${isDarkTheme && styles.dark}`}>
-      <div className={styles.toggle}>
-        <button className={styles.loginBtn} onClick={() => handleClick("login")}>Login</button> / <button className={styles.loginBtn} onClick={() => handleClick("register")}>Signup</button>
-      </div>
+      <LoginLinks />
 
       <ThemeToggle />
       <CardContainer />
